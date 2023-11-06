af6a67a21f8720f7bd518470ff62902bab0338a0
kerimberdi <kerimberdi99@gmail.com>
2023-11-06 19:53:52 +0500

    asd

--- main.py
+++ main.py
@@ -8,16 +8,4 @@ service = webdriver.ChromeService(executable_path=chromedriver_path)
 driver = webdriver.Chrome(service=service)
 
 parser = PartsParser(driver)
-
-
-# while True:
-try:
-    parser.parse()
-except:
-    driver.quit()
-    driver = webdriver.Chrome(service=service)
-
-    danger('Something went wrong')
-    parser = PartsParser(driver)
-
-    parser.parse()
+parser.parse()
